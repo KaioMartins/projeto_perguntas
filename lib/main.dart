eed1f12e1788bf218f1c7138e9f0eeb614d70245
Kaio Martins <kaio.ccmartins@gmail.com>
2022-07-17 15:52:06 -0300

    Changing the app form StatelessWidget to StatefulWidget

--- lib/main.dart
+++ lib/main.dart
@@ -2,39 +2,49 @@ import 'package:flutter/material.dart';
 
 main() => runApp(PerguntaApp());
 
-@override
-class PerguntaApp extends StatelessWidget {
+class PerguntaAppState extends State<PerguntaApp> {
+  var perguntaSelecionada = 0;
+
   void responder() {
-    print('Pergunta respondida!');
+    setState(() {
+      perguntaSelecionada++;
+    });
+    print('perguntaSelecionada');
   }
 
-  final perguntas = [
-    'Qual é sua cor favorita?',
-    'Qual é o seu animal favorito?',
-  ];
-
+  @override
   Widget build(BuildContext context) {
+    final perguntas = [
+      'Qual é sua cor favorita?',
+      'Qual é o seu animal favorito?',
+    ];
+
     return MaterialApp(
         home: Scaffold(
             appBar: AppBar(
               title: Text('Perguntas'),
             ),
             body: Column(children: <Widget>[
-              Text(perguntas[0]),
+              Text(perguntas[perguntaSelecionada]),
               ElevatedButton(
                 child: Text('Resposta 1'),
                 onPressed: responder,
               ),
               ElevatedButton(
                 child: Text('Resposta 2'),
-                onPressed: () {
-                  print('Resposta 2 foi selecionada!');
-                },
+                onPressed: responder,
               ),
               ElevatedButton(
                 child: Text('Resposta 3'),
-                onPressed: () => print('Resposta 3!!!'),
+                onPressed: responder,
               )
             ])));
+  }
+}
+
+class PerguntaApp extends StatefulWidget {
+  @override
+  PerguntaAppState createState() {
+    return PerguntaAppState();
   }
 }
