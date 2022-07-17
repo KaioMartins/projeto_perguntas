29ff737c435fcd06fed7257807e0d8f729ec4535
Kaio Martins <kaio.ccmartins@gmail.com>
2022-07-17 17:32:16 -0300

    Calling the class resposta on main

--- lib/resposta.dart
+++ lib/resposta.dart
@@ -3,15 +3,16 @@ import 'package:flutter/material.dart';
 
 class Resposta extends StatelessWidget {
   final String texto;
+  final void Function() quandoSelecionado;
 
-  Resposta(this.texto);
+  Resposta(this.texto, this.quandoSelecionado);
 
   @override
   Widget build(BuildContext context) {
     return Container(
       width: double.infinity,
       child: ElevatedButton(
-        onPressed: () {},
+        onPressed: quandoSelecionado,
         child: Text(
           texto,
           style: TextStyle(fontSize: 20),
